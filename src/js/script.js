58ad18167eba3989ebee4731fc8d49e3eb8cf616
ermakovasn <sveta.kapitsa@gmail.com>
2023-01-07 12:33:47 +0100

    add adaptive catalog

--- src/js/script.js
+++ src/js/script.js
@@ -7,13 +7,68 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
+            breakpoint: 320,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+      }
+    },
+                {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+      }
+    },
+            {
                 breakpoint: 992,
                 settings: {
                     dots: true,
                     arrows: false
                 }}
         ]
+        
       });
+    //   настраиваем табы
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+// оживляем каждую кнопку подробнее с помощью функции each
+    //   $('.catalog-item__link').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         // отменяем стандартное поведение браузера при кликании по ссылке
+    //         e.preventDefault();
+    //         // команда переключения класса: если класс есть, то он убирается, если его нет, то он добавляется
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    //   });
+
+    //   $('.catalog-item__back').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         // отменяем стандартное поведение браузера при кликании по ссылке
+    //         e.preventDefault();
+    //         // команда переключения класса: если класс есть, то он убирается, если его нет, то он добавляется
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    //   }) 
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                // отменяем стандартное поведение браузера при кликании по ссылке
+                e.preventDefault();
+                // команда переключения класса: если класс есть, то он убирается, если его нет, то он добавляется
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+          });
+      };
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
   });
 
 // tiny-slider
@@ -41,3 +96,4 @@ $(document).ready(function(){
 //   });
 
 //   переименовываем в css на next and prev, выравниваем картинки по центру в файле scss
+
